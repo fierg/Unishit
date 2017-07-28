b9a08fc843fafa673568b16c32d48560517d0999
fierg <sven.fiergolla@gmail.com>
2017-07-28 19:13:16 +0200

    progress

--- Kleines Studienprojekt/DebuggingProfiling/src/com/unitrier/teaching/profiling/home/CollectionTest.java
+++ Kleines Studienprojekt/DebuggingProfiling/src/com/unitrier/teaching/profiling/home/CollectionTest.java
@@ -1,5 +1,13 @@
 package com.unitrier.teaching.profiling.home;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -44,13 +52,13 @@ public final class CollectionTest
 
         List<Long> times = new LinkedList<>();
 
-
         if(repetitions < 0){
             repetitions = getEstimatedRepetitions(len, list);
         }
 
-        System.out.printf("Using\t %s\tRepetitions: %d\n",
-                collectionType.toString(), repetitions);
+
+        System.out.printf("Using %s\t Repetitions: %d \tPart: %f\n",
+                collectionType.toString(), repetitions, part);
 
 
 
@@ -58,7 +66,7 @@ public final class CollectionTest
         {
             long start = System.currentTimeMillis();
 
-            int index = (int) (Math.random() * len);
+            int index = (int) (Math.random() * len* part);
             list.add(list.remove(index));
 
             times.add(System.currentTimeMillis()-start);
@@ -92,16 +100,16 @@ public final class CollectionTest
         testCollection();
 
         out.println("\nprofiling() withour part\n");
-            profiling(1);
+        //    profiling();
 
         out.println("\nprofiling() with part\n");
-
+        profilingIncrementalRiseOfPart(1000000,0.01 , 0.01);
     }
 
     private static void testCollection() {
         CollectionTypeEnum[] values = CollectionTypeEnum.values();
         int listLength = 100;
-        long repetitions = 10000000;
+        long repetitions = 1000000;
 
         CollectionTypeEnum collectionType = values[2];
 
@@ -117,11 +125,11 @@ public final class CollectionTest
                 totalNanos / 1E6, (double) totalNanos / size, (double) totalNanos / (1E6 * size));
     }
 
-    public static void profiling(double part){
+    public static void profiling(){
         int listLength = 1000;
         for (CollectionTypeEnum collectionType: CollectionTypeEnum.values()) {
 
-            List<Long> times = runCollectionTest(collectionType, listLength,-1, part);
+            List<Long> times = runCollectionTest(collectionType, listLength,-1, 1);
 
             long totalNanos = times.stream().mapToLong(Long::longValue).sum();
 
@@ -134,4 +142,35 @@ public final class CollectionTest
         }
 
         }
+    public static void profilingIncrementalRiseOfPart(long iterations, double startValuePart, double incStepp){
+        int listLength = 1000;
+
+        for (CollectionTypeEnum collectionType: CollectionTypeEnum.values()) {
+            List<String> totals = new LinkedList<String>();
+
+
+            for(double part = startValuePart; part <= 1; part += incStepp){
+
+
+            List<Long> times = runCollectionTest(collectionType, listLength,iterations, part);
+
+            long totalNanos = times.stream().mapToLong(Long::longValue).sum();
+
+            totals.add(String.valueOf(totalNanos));
+
+
+        }
+
+        String path = "data/"+ collectionType.toString() +".csv";
+        Path file = Paths.get(path);
+            try(FileWriter fileWriter = new FileWriter(file.toFile())) {
+                fileWriter.write(totals.toString().substring(1,totals.toString().length()-1));
+                //Files.write(file, totals, Charset.forName("UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+
+
+        }
+
     }
+    }}
